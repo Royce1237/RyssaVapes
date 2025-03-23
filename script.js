2f7b26bcd37b841b0aad898b20993443241c8174
RyssaVapes <saulilloelmasgordillo@gmail.com>
2025-03-23 00:10:32 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Esto es solo un ejemplo básico
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+        alert('Producto agregado al carrito');
+    });
+});
